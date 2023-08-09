841166f3d5cf808b43a458888919dc67c04d1ff7
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-10 02:27:26 +0300

    feat: adds section questions

---
+++ src/components/pages/home/questions/data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    question: 'How to start investing',
+    text: [
+      `Sed in sem pellentesque, eleifend erat vitae, tincidunt turpis. Pellentesque augue elit, rutrum sit amet commodo vel, porta quis purus. Nullam faucibus, odio eu bibendum auctor, eros dolor aliquet purus, sit amet pellentesque neque est id tortor. In egestas, erat a dapibus dapibus, mi augue ornare lorem, nec pretium ligula purus nec tellus. Aenean a scelerisque nisi. Nullam pretium fermentum nunc, feugiat placerat urna rutrum eget. Nullam mattis justo consequat risus. Donec imperdiet enim id efficitur pharetra. Nam sollicitudin at est eget auctor. Ut sed ante quis nulla tincidunt auctor. Quisque accumsan pretium tincidunt. Etiam viverra ultrices est vitae suscipit. Pellentesque elementum purus non nisl pharetra consequat. Nunc in venenatis orci.`,
+    ],
+  },
+  {
+    question: 'How much money is needed to start investing?',
+
+    text: [
+      `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis odio commodi quod iste inventore dolor dolorum quasi omnis quo vero eaque iusto, voluptatum blanditiis soluta similique explicabo, sapiente porro vel.`,
+      `Lorem, ipsum dolor sit amet consectetur adipisicing elit.`,
+    ],
+  },
+  {
+    question: 'Is the Midas.Investments secure?',
+
+    text: [
+      `Labore, tenetur, quaerat ab, quas quisquam voluptas rem explicabo ratione accusantium ut nihil voluptatibus dolores natus nobis laborum odit nisi suscipit maxime.`,
+      `Labore, tenetur, quaerat ab, quas quisquam voluptas rem explicabo ratione accusantium utnihil voluptatibus dolores natus nobis laborum odit nisi suscipit maxime.`,
+    ],
+  },
+  {
+    question: 'How are funds withdrawn from the platform?',
+    text: [
+      `First, you must select the wallet address where the funds will be transferred: this can be the address of an external exchange, local wallet or other service.`,
+      `Open your investment unit card and find there the field for the withdrawal address, and next to it the "EDIT" button.`,
+      `Once you have provided a withdrawal address, you need to confirm the withdrawal address by email. The address will change only after your confirmation.`,
+    ],
+  },
+  {
+    question: 'How Midas.Investments generates yield for its customers?',
+    text: [
+      `Labore, tenetur, quaerat ab, quas quisquam voluptas rem explicabo ratione accusantium ut nihil voluptatibus dolores natus nobis laborum odit nisi suscipit maxime.`,
+      `Labore, tenetur, quaerat ab, quas quisquam voluptas rem explicabo ratione accusantium ut nihil voluptatibus dolores natus nobis laborum odit nisi suscipit maxime.  `,
+    ],
+  },
+  {
+    question: 'What is Midas token and what are its values?',
+    text: [
+      `Labore, tenetur, quaerat ab, quas quisquam voluptas rem explicabo ratione accusantium ut nihil voluptatibus dolores natus nobis laborum odit nisi suscipit maxime. `,
+    ],
+  },
+];
---
+++ src/components/pages/home/questions/questions.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import { Accordion } from 'hooks/accordion/accordion';
+
+import { data } from './data';
+
+import styles from './questions.module.scss';
+
+import telegram from '../../../../images/telegram.svg';
+import discord from '../../../../images/discord.svg';
+import support from '../../../../images/support.svg';
+
+const cx = classNames.bind(styles);
+
+const Questions = () => (
+  <section className={cx('questions')}>
+    <div className={cx('container', 'container-medium')}>
+      <h2 className={cx('title')}>Frequently asked questions</h2>
+      <p className={cx('text')}>
+        Everything you need to know about the Midas. Can’t find the answer you’re looking for? Feel
+        free to contact me directly via:
+      </p>
+      <ul className={cx('list-links')}>
+        <li className={cx('item')}>
+          <a href="#" className={cx('link')}>
+            <img src={telegram} alt="Telegram" className={cx('image')} />
+            <span>Telegram</span>
+          </a>
+        </li>
+        <li className={cx('item')}>
+          <a href="#" className={cx('link')}>
+            <img src={discord} alt="Discord" className={cx('image')} />
+            <span>Discord</span>
+          </a>
+        </li>
+        <li className={cx('item')}>
+          <a href="#" className={cx('link')}>
+            <img src={support} alt="Support" className={cx('image')} />
+            <span>Support</span>
+          </a>
+        </li>
+      </ul>
+      <ul className={cx('list')}>
+        {data.map((section, index) => (
+          <Accordion key={index} section={section} />
+        ))}
+      </ul>
+    </div>
+  </section>
+);
+
+export default Questions;
