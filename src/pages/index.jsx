5d4f7b74f22294566bc6984276ec89d8df290263
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-11 01:35:31 +0300

    feat: font styles , section for global page and package

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
+import Hero from 'components/pages/home/hero';
+import StakeMidas from 'components/pages/home/stakeMidas';
+import Questions from 'components/pages/home/questions';
 
-const HomePage = () => <Layout>Hello world!</Layout>;
+const HomePage = () => (
+  <Layout>
+    <Hero />
+    <StakeMidas />
+    <Questions />
+  </Layout>
+);
 
 export default HomePage;
 
