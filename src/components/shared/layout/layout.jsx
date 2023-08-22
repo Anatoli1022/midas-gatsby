303ec18b71f34a5be5d6217edfeecfccccc85034
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-22 17:26:00 +0300

    feat: adds footer in layout

--- src/components/shared/layout/layout.jsx
+++ src/components/shared/layout/layout.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import styles from './layout.module.scss';
+import Footer from '../footer/Footer';
 
 const cx = classNames.bind(styles);
 
 const Layout = ({ children }) => (
   <div className={cx('wrapper')}>
     <main className={cx('content')}>{children}</main>
+    <Footer />
   </div>
 );
 
