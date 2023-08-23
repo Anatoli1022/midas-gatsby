b8cabf6702f1fc178359aaa0c59f47d6f5ab9fea
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-23 19:12:29 +0300

    feat: hero

---
+++ src/components/pages/home/hero/Hero.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import classNames from 'classnames/bind';
+import styles from './Hero.module.scss';
+import SplitColumns from './animationСomponents/splitColumns';
+import ButtonAnimation from './animationСomponents/buttonAnimation';
+
+const cx = classNames.bind(styles);
+
+const Hero = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+  });
+  return (
+    <section className={cx('hero')}>
+      <div className={cx('container')}>
+        <h1 className={cx('title')} ref={ref}>
+          Unlock the full potential of Midas ecosystem
+          <span className={cx('title-span')}>Buy midas token</span>
+        </h1>
+        <div className={cx('content-wrapper')}>
+          <div className={cx('content')}>
+            <SplitColumns inView={inView} />
+            <div className={cx('text-wrapper')}>
+              <p className={cx('text')}>Stake MIDAS token and earn 30% APY</p>
+              <a href="#" className={cx('link')}>
+                Read more
+              </a>
+            </div>
+          </div>
+          <div className={cx('content')}>
+            <ButtonAnimation inView={inView} />
+            <div className={cx('text-wrapper')}>
+              <p className={cx('text')}>Get increased APY by getting rewards in MIDAS</p>
+              <a href="#" className={cx('link')}>
+                How it work?
+              </a>
+            </div>
+          </div>
+          <div className={cx('content')}>
+            <div className={cx('block-animation')}></div>
+            <div className={cx('text-wrapper')}>
+              <p className={cx('text')}>Buybacks & burns supporting MIDAS growth</p>
+              <a href="#" className={cx('link')}>
+                How it work?
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
