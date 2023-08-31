ed3955caea428115b8ce702d5aa8bf569c4de867
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-01 00:36:57 +0300

    feat: adds new section header

---
+++ src/components/shared/header/Header.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+import Menu from 'hooks/menu/menu';
+import { Link } from 'gatsby';
+
+const cx = classNames.bind(styles);
+
+const Header = () => {
+  return (
+    <section className={cx('header')}>
+      <div className={cx('container')}>
+        <div className={cx('navigation')}>
+          <div className={cx('left-block')}>
+            <a href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="56"
+                height="64"
+                viewBox="0 0 56 64"
+                fill="none"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M35.875 21L0 43H41L35.875 21ZM5.125 21L17.0791 29.998L0 43L5.125 21Z"
+                  fill="white"
+                />
+              </svg>
+            </a>
+            <ul className={cx('list')}>
+              <li>
+                <Link to="" className={cx('link')}>
+                  Platform
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={cx('link')}>
+                  Midas Coin
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={cx('link')}>
+                  YAPs
+                </Link>
+              </li>
+              <li>
+                <Link to="" className={cx('link')}>
+                  Resources
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Link to="" className={cx('link')}>
+              Aurielien Solamon
+            </Link>
+            <Menu />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Header;
