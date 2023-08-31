9346e48610b372e1a9be2ea085d19d32457ad9de
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-01 00:38:56 +0300

    feat: adds ParallaxProvider in index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { ParallaxProvider } from 'react-scroll-parallax';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 import Hero from 'components/pages/home/hero';
@@ -7,17 +7,21 @@ import TokenMetrics from 'components/pages/home/tokenMetrics';
 import StakeMidas from 'components/pages/home/stakeMidas';
 import MidasToken from 'components/pages/home/midasToken';
 import Support from 'components/pages/home/support';
+import BuyMidas from 'components/pages/home/buyMidas';
 import Questions from 'components/pages/home/questions';
 
 const HomePage = () => (
-  <Layout>
-    <Hero />
-    <TokenMetrics />
-    <StakeMidas />
-    <MidasToken />
-    <Support />
-    <Questions />
-  </Layout>
+  <ParallaxProvider>
+    <Layout>
+      <Hero />
+      <TokenMetrics />
+      <StakeMidas />
+      <MidasToken />
+      <Support />
+      <BuyMidas />
+      <Questions />
+    </Layout>
+  </ParallaxProvider>
 );
 
 export default HomePage;
